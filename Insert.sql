f6965bb8ff7513729c3ff480eec76b1236971d92
NavyaPuchakayala22 <navya.puchakayala22@gmail.com>
2026-02-12 12:33:12 +0530

    Documented and uploaded Department and Employee Data used grouping aggregations case statements joins subqueries

---
+++ Insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Departments VALUES
+		(1,'HR','Hyderabad'),
+        (2,'IT','Bangalore'),
+        (3,'Finance','Chennai'),
+        (4,'Marketing','Mumbai');
+        
+INSERT INTO employees VALUES
+		(101,'Anita',50000,'2021-05-10',2),
+        (102,'Rahul',60000,'2020-03-15',2),
+        (103,'Sita',45000,'2022-07-01',1),
+        (104,'Kiran',70000,'2019-01-20',3),
+        (105,'Meena',NULL,'2023-02-10',4);
